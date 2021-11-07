31dbb0727a411819d32b07f390f697b9b157f693
Darren Barklie <darrenbarklie@gmail.com>
2021-11-07 20:03:08 +0000

    Chapter 4 complete

---
+++ 04/try-react/src/AnotherScreen.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const AnotherScreen: FC = () => {
+  return <div>Hellow! Another screen</div>;
+};
+
+export default AnotherScreen;
---
+++ 04/try-react/src/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Home: FC = () => {
+  return <div>Hello world! Home</div>;
+};
+
+export default Home;
